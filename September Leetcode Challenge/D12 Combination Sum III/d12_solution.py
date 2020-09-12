1b8178f888da6b9897b530645c8965824cc5c5fa
ayushi7rawat <ayushi7rawat@gmail.com>
2020-09-12 14:02:07 +0530

    adding youtube video

--- September Leetcode Challenge/D12 Combination Sum III/d12_solution.py
+++ September Leetcode Challenge/D12 Combination Sum III/d12_solution.py
@@ -4,3 +4,18 @@ Problem Name: Maximum Product Subarray
 Author: Ayushi Rawat
 Date: 12-09-2020
 '''
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+            res = []
+            
+            def helper(nums, k, n, index, path, res):
+                if k < 0  or n < 0:
+                    return
+                if k == 0 and n == 0:
+                    res.append(path)
+                    
+                for i in range(index, len(nums)):
+                    helper(nums, k-1, n-nums[i], i+1, path+[nums[i]], res)
+                
+            helper(range(1,10), k, n, 0, [], res)
+            
+            return res
