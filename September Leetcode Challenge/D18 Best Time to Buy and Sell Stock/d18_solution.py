df5e6c50700cff60eed3e7cd91a656da776d223b
ayushi7rawat <ayushi7rawat@gmail.com>
2020-09-18 14:39:48 +0530

    Robot Bounded In Circle

--- September Leetcode Challenge/D18 Best Time to Buy and Sell Stock/d18_solution.py
+++ September Leetcode Challenge/D18 Best Time to Buy and Sell Stock/d18_solution.py
@@ -4,4 +4,14 @@ Problem Name: Best Time to Buy and Sell Stock
 Author: Ayushi Rawat
 Date: 18-09-2020
 '''
-    
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float(inf)
+        max_profit = 0
+        
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                temp = price - min_price
+                max_profit = max(max_profit, temp)
+        return max_profit
