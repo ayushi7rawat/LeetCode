7051b03c41a005c37672a03d6d8a24705959b6e3
Ayushi Rawat <ayushi7rawat@gmail.com>
2021-02-10 10:02:19 +0530

    771. Jewels and Stones

---
+++ Problem code submissions/1748. Sum of Unique Elements.py
@@ -0,0 +1,15 @@
+class Solution:
+#1748. Sum of Unique Elements
+    def sumOfUnique(self, nums: List[int]) -> int:
+        s = {}
+        for i in nums:
+          if i in s:
+            s[i] += 1
+          else:
+            s[i] = 1
+        
+        res = 0
+        for i in s:
+          if s[i]==1:
+            res += i
+        return res(a)
