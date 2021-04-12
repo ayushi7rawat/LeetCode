93958b72fc4dd089b9f65cb5e74ef07844749daa
Ayushi Rawat <ayushi7rawat@gmail.com>
2021-04-12 10:50:10 +0530

    1822. Sign of the Product of an Array

---
+++ Weekly contest/weekly contest 236/1822. Sign of the Product of an Array.py
@@ -0,0 +1,28 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        prod = 1
+		
+        for i in range(len(nums)):
+            prod *= nums[i]
+            
+        if prod > 0:
+            return 1
+        elif prod == 0:
+            return 0
+        else:
+            return -1
+			
+			
+			
+			
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        if 0 in nums:
+            return 0
+			
+        count = 0
+        for i in nums:
+            if i < 0:
+                count += 1
+        return 1 if count%2 == 0 else -1
+            
