db37e86aec3a0e6b2748141e5e4da28e090bb56b
Ayushi Rawat <ayushi7rawat@gmail.com>
2021-05-02 09:27:31 +0530

    5746. Minimum Distance to the Target Element

--- Weekly contest/Weekly contest 239/5746. Minimum Distance to the Target Element.py
+++ Weekly contest/Weekly contest 239/5746. Minimum Distance to the Target Element.py
@@ -2,27 +2,24 @@
 5746. Minimum Distance to the Target Element
 Author: Ayushi Rawat
 
-Given an integer array nums (0-indexed) and two integers target and start, find an index i such that nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x.
-
+Given an integer array nums (0-indexed) and two integers target and start, find an index i such that nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x
 Return abs(i - start).
-
 It is guaranteed that target exists in nums.
 
- 
 
 Example 1:
-
 Input: nums = [1,2,3,4,5], target = 5, start = 3
 Output: 1
 Explanation: nums[4] = 5 is the only value equal to target, so the answer is abs(4 - 3) = 1.
 
-Constraints:
 
+Constraints:
 1 <= nums.length <= 1000
 1 <= nums[i] <= 104
 0 <= start < nums.length
 target is in nums.
 '''
+
 
 class Solution:
     def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
