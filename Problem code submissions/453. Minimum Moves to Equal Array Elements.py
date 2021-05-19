815c1a11424789b0f1c438be0a47ed36ce929502
Ayushi Rawat <ayushi7rawat@gmail.com>
2021-05-19 22:54:42 +0530

    imar@AyushiRawat MINGW64 /f/GIT/Leetcode (master) $ git add .
    imar@AyushiRawat MINGW64 /f/GIT/Leetcode (master)
    $ git commit -m

---
+++ Problem code submissions/453. Minimum Moves to Equal Array Elements.py
@@ -0,0 +1,31 @@
+class Solution:
+    #gives timeout error
+    def minMoves(self, nums: List[int]) -> int:
+	'''
+	453. Minimum Moves to Equal Array Elements
+    By: Ayushi Rawat
+	'''
+        counter = 0
+        curr_min = min(nums)
+        curr_max = max(nums)
+
+        while curr_max != curr_min:
+            counter += 1
+            nums.sort()
+            
+            for i in range(len(nums)-1):
+                nums[i] += 1
+
+            curr_min = min(nums)
+            curr_max = max(nums)    
+        return counter
+        
+    #second solution
+    def minMoves(self, nums: List[int]) -> int:
+        _sum = 0
+        _min = min(nums)
+        
+        for i in range(len(nums)):
+            _sum += (nums[i] - _min)
+            
+        return _sum
